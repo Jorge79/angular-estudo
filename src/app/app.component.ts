89084506def27d0c12321957eb174c0c0495a3ab
Jorge Francisco <Jorge7.9@hotmail.com>
2022-06-02 01:42:14 -0300

    Ciclo de vida do componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'teste';
+  valor: number = 5;
+  deletarCiclo: boolean = false;
+
+  destruirCiclo() {
+    this.deletarCiclo = true;
+  }
+
+  mudarValor() {
+    this.valor++;
+  }
 }
